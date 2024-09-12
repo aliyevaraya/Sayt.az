32e7d498075c6d8fbe3151bf3c8c04887373d05e
Raya Aliyeva <73crd54@code.edu.az>
2024-09-12 21:58:47 +0400

    some changes

--- app.js
+++ app.js
@@ -1,3 +1,195 @@
+const data = {
+  costLogo: [
+    {
+      id: 7,
+      name: "Custom",
+      price: 200,
+      type: "logo_design",
+    },
+    {
+      id: 15,
+      name: "No need",
+      price: 0,
+      type: "logo_design",
+    },
+    {
+      id: 20,
+      name: "Simple",
+      price: 100,
+      type: "logo_design",
+    },
+  ],
+  costAddon: [
+    {
+      id: 1,
+      name: "Blog",
+      price: 200,
+      type: "website_addons",
+    },
+    {
+      id: 2,
+      name: "Member login /‍‍‍ Profiles",
+      price: 400,
+      type: "website_addons",
+    },
+    {
+      id: 8,
+      name: "Responsive",
+      price: 100,
+      type: "website_addons",
+    },
+    {
+      id: 22,
+      name: "Forum",
+      price: 400,
+      type: "website_addons",
+    },
+    {
+      id: 23,
+      name: "Messaging",
+      price: 50,
+      type: "website_addons",
+    },
+    {
+      id: 24,
+      name: "Live Chat",
+      price: 50,
+      type: "website_addons",
+    },
+    {
+      id: 25,
+      name: "Gallery",
+      price: 100,
+      type: "website_addons",
+    },
+    {
+      id: 26,
+      name: "Contact Form",
+      price: 100,
+      type: "website_addons",
+    },
+    {
+      id: 27,
+      name: "On-site Search",
+      price: 100,
+      type: "website_addons",
+    },
+    {
+      id: 28,
+      name: "Map Data / Geolocation",
+      price: 100,
+      type: "website_addons",
+    },
+    {
+      id: 29,
+      name: "Booking",
+      price: 400,
+      type: "website_addons",
+    },
+    {
+      id: 30,
+      name: "Social Media integration",
+      price: 50,
+      type: "website_addons",
+    },
+    {
+      id: 31,
+      name: "Analytics / Tracking",
+      price: 50,
+      type: "website_addons",
+    },
+    {
+      id: 36,
+      name: "Admin Panel",
+      price: 300,
+      type: "website_addons",
+    },
+  ],
+  costSeo: [
+    {
+      id: 3,
+      name: "Titles & Meta",
+      price: 200,
+      type: "seo_addons",
+    },
+    {
+      id: 32,
+      name: "Keyword research",
+      price: 300,
+      type: "seo_addons",
+    },
+    {
+      id: 33,
+      name: "+20 Advanced techniques",
+      price: 300,
+      type: "seo_addons",
+    },
+    {
+      id: 34,
+      name: "Header Tags",
+      price: 100,
+      type: "seo_addons",
+    },
+    {
+      id: 35,
+      name: "Sitemap",
+      price: 100,
+      type: "seo_addons",
+    },
+  ],
+  costSetting: [
+    {
+      id: 18,
+      name: "min",
+      price: 450,
+      type: "setting",
+    },
+    {
+      id: 19,
+      name: "page",
+      price: 20,
+      type: "setting",
+    },
+  ],
+  costEcommerce: [
+    {
+      id: 38,
+      name: "Payment",
+      price: 200,
+      type: "ecommerce",
+    },
+    {
+      id: 39,
+      name: "Add to cart",
+      price: 500,
+      type: "ecommerce",
+    },
+    {
+      id: 40,
+      name: "Products",
+      price: 300,
+      type: "ecommerce",
+    },
+    {
+      id: 41,
+      name: "Favourite",
+      price: 300,
+      type: "ecommerce",
+    },
+    {
+      id: 42,
+      name: "Filter",
+      price: 300,
+      type: "ecommerce",
+    },
+    {
+      id: 43,
+      name: "Description",
+      price: 200,
+      type: "ecommerce",
+    },
+  ],
+};
 //// Sidebar /////
 const sidebar = document.getElementById("sidebar");
 
@@ -32,23 +224,55 @@ function openDropDown(list) {
 }
 ///// Calculator //////
 
-const pages = document.getElementById("pages");
-const additions = document.querySelectorAll(".addition");
-const seo = document.querySelectorAll(".seo");
-const etrade = document.querySelectorAll(".etrade");
-const pageRange = document.getElementById("pageRange");
+const elaveler = document.getElementById("elaveler");
+const seo = document.getElementById("seo");
+const eticaret = document.getElementById("eticaret");
+function addElement() {
+  data.costAddon.map((item) => {
+    elaveler.innerHTML += `
+          <label for="${item.name}" class="relative flex justify-between items-center cursor-pointer">
+            <p class="w-3/4 text-sm font-medium text-gray-900">${item.name}</p>
+            <div class="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
+                <span class="relative">
+                    <input onchange="addAddition()" value="${item.price}" id="${item.name}" type="checkbox" class="hidden peer addition">
+                    <div class="w-10 h-6 rounded-full shadow-inner peer-checked:bg-violet-600 bg-gray-200"></div>
+                    <div class="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto bg-white"></div>
+                </span>
+            </div>
+        </label>
+      `;
+  });
+
+  data.costSeo.map((item) => {
+    seo.innerHTML += `
+      <label class="flex items-center gap-3 cursor-pointer">
+          <input onchange="addSeo()" class="seoInp h-5 w-5 rounded-md border border-gray-400 accent-[#a271f2] cursor-pointer" value="${item.price}" type="checkbox" />
+          ${item.name}
+      </label>
+    `;
+  });
+
+  data.costEcommerce.map((item) => {
+    eticaret.innerHTML += `
+      <label for="${item.name}" class="relative flex justify-between items-center cursor-pointer">
+          <p class="w-3/4 text-sm font-medium text-gray-900">${item.name}</p>
+          <div class="inline-flex items-center space-x-4 cursor-pointer dark:text-gray-800">
+              <span class="relative">
+                  <input onchange="addEtrade()" value="${item.price}" id="${item.name}" type="checkbox" class="etrade hidden peer">
+                  <div class="w-10 h-6 rounded-full shadow-inner peer-checked:bg-violet-600 bg-gray-200"></div>
+                  <div class="absolute inset-y-0 left-0 w-4 h-4 m-1 rounded-full shadow peer-checked:right-0 peer-checked:left-auto bg-white"></div>
+              </span>
+          </div>
+      </label>
+    `;
+  });
+}
+
+addElement();
+
+const pagepriceForSeoRange = document.getElementById("pageRange");
 const dizaynRange = document.getElementById("dizaynRange");
 const logoRange = document.getElementById("logoRange");
-const priceForPages = document.getElementById("priceForPages");
-const priceForAddition = document.getElementById("priceForAddition");
-const priceForSeo = document.getElementById("priceForSeo");
-const priceForEtrade = document.getElementById("priceForEtrade");
-const priceForLogo = document.getElementById("priceForLogo");
-const allPages = document.getElementById("allPages");
-const allAdditions = document.getElementById("allAdditions");
-const allSeo = document.getElementById("allSeo");
-const allEtrade = document.getElementById("allEtrade");
-const allLogo = document.getElementById("allLogo");
 const allPrice = document.getElementById("allPrice");
 
 let pagePrice = 400;
@@ -58,51 +282,55 @@ let seoSum = 0;
 let etradeSum = 0;
 let legoSum = 0;
 
+const pages = document.getElementById("pages");
+const priceForPages = document.querySelectorAll(".priceForPages");
 function getPriceForPages() {
-  let sum = 100 * pageRange.value * dizaynRange.value;
-  pages.innerHTML = pageRange.value;
-  priceForPages.innerHTML = allPages.innerHTML = sum;
+  let sum = pageRange.value * dizaynRange.value;
+  pages.innerHTML = pageRange.value / 100;
+  priceForPages.forEach((item) => (item.innerHTML = sum));
   pagePrice = sum;
   getAllPrice();
 }
+
+const priceForLogo = document.querySelectorAll(".priceForLogo");
 function getPriceForLogo() {
-  let sum = 100 * logoRange.value;
-  priceForLogo.innerHTML = logoRange.value;
-  priceForLogo.innerHTML = allLogo.innerHTML = sum;
-  logoPrice = sum;
+  priceForLogo.forEach((item) => (item.innerHTML = logoRange.value));
+  logoPrice = logoRange.value;
   getAllPrice();
 }
 
-let additionArr = Array.from(additions);
-let seoArr = Array.from(seo);
-let etradeArr = Array.from(etrade);
-
+const priceForAddition = document.querySelectorAll(".priceForAddition");
+const additions = document.querySelectorAll(".addition");
 function addAddition() {
-  let sum = additionArr.reduce(
-    (sum, item) => sum + +(item.checked ? item.value : 0),
-    0
-  );
-  priceForAddition.innerHTML = allAdditions.innerHTML = sum;
+  let sum = 0;
+  additions.forEach((item) => {
+    if (item.checked) sum += +item.value;
+  });
+  priceForAddition.forEach((item) => (item.innerHTML = sum));
   addSum = sum;
   getAllPrice();
 }
 
+const priceForSeo = document.querySelectorAll(".priceForSeo");
+const seoInp = document.querySelectorAll(".seoInp");
 function addSeo() {
-  let sum = seoArr.reduce(
-    (sum, item) => sum + +(item.checked ? item.value : 0),
-    0
-  );
-  priceForSeo.innerHTML = allSeo.innerHTML = sum;
+  let sum = 0;
+  seoInp.forEach((item) => {
+    if (item.checked) sum += +item.value;
+  });
+  priceForSeo.forEach((item) => (item.innerHTML = sum));
   seoSum = sum;
   getAllPrice();
 }
 
+const priceForEtrade = document.querySelectorAll(".priceForEtrade");
+const etrade = document.querySelectorAll(".etrade");
 function addEtrade() {
-  let sum = etradeArr.reduce(
-    (sum, item) => sum + +(item.checked ? item.value : 0),
-    0
-  );
-  priceForEtrade.innerHTML = allEtrade.innerHTML = sum;
+  let sum = 0;
+  etrade.forEach((item) => {
+    if (item.checked) sum += +item.value;
+  });
+  priceForEtrade.forEach((item) => (item.innerHTML = sum));
   etradeSum = sum;
   getAllPrice();
 }
@@ -115,7 +343,7 @@ function getAllPrice() {
 
 ///// POPUP ///////
 
-const bgBlur = document.getElementById('bgBlur')
+const bgBlur = document.getElementById("bgBlur");
 let flag = true;
 
 function handlePopup() {
@@ -123,9 +351,9 @@ function handlePopup() {
   flag = !flag;
 }
 function closePopup() {
-  bgBlur.style.display = "none"
+  bgBlur.style.display = "none";
   flag = !flag;
 }
 function preventClick(e) {
-  e.stopPropagation()
+  e.stopPropagation();
 }
